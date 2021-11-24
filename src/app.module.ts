5a645a2c3ef5c0095500f0b1b61aaf65bd5d564a
jsonLiang <yuanyu90221@gmail.com>
2021-11-24 17:22:46 +0800

    generate tasks and auth module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,7 @@
 import { Module } from '@nestjs/common';
+import { TasksModule } from './tasks/tasks.module';
+import { AuthModule } from './auth/auth.module';
 @Module({
-  imports: [],
+  imports: [TasksModule, AuthModule],
 })
 export class AppModule {}
